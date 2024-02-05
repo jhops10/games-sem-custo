05ef8f50a782bb4ed25b47b23ffbcd48fe9cd788
João Paulo <joaowais@gmail.com>
2024-02-05 16:30:32 -0300

    projeto finalizado

--- src/app/pages/all-games/all-games.component.ts
+++ src/app/pages/all-games/all-games.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GamesService } from '../../services/games.service';
+import { Game } from '../../models/game.model';
 
 @Component({
   selector: 'app-all-games',
   templateUrl: './all-games.component.html',
-  styleUrl: './all-games.component.scss'
+  styleUrl: './all-games.component.scss',
 })
-export class AllGamesComponent {
+export class AllGamesComponent implements OnInit {
+  gamesData: Game[] = [];
 
+  constructor(private gameService: GamesService) {}
+
+  ngOnInit(): void {
+    this.gameService.getAllGames().subscribe((data) => {
+      this.gamesData = this.gamesData.concat(data);
+    });
+  }
 }
